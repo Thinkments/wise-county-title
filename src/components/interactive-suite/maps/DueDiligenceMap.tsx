26ab7d2cc44e7b75c4a69edf70cb14c66a140dc7
unknown <thinkments@gmail.com>
2026-09-02 17:21:06 -0500

    Update DueDiligenceMap with OpenStreetMap clean tile layer

--- src/components/interactive-suite/maps/DueDiligenceMap.tsx
+++ src/components/interactive-suite/maps/DueDiligenceMap.tsx
@@ -46,10 +46,11 @@ export default function DueDiligenceMap() {
       if (!mapInstanceRef.current && mapContainerRef.current) {
         const map = L.map(mapContainerRef.current).setView([33.2348, -97.64], 11);
         
-        L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-          attribution: '&copy; <a href="https://carto.com/">CARTO</a> &copy; OpenStreetMap',
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
           maxZoom: 19,
         }).addTo(map);
+
 
         const markersGroup = L.layerGroup().addTo(map);
         markersGroupRef.current = markersGroup;
